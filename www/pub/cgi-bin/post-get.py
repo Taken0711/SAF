55ebc97316f1816c006c7bae7e3d48280e51cddb
Jeremy Junac <jeremy.junac@etu.unice.fr>
2017-05-06 18:18:27 +0200

    bugfix when query string isn't define

--- www/pub/cgi-bin/post-get.py
+++ www/pub/cgi-bin/post-get.py
@@ -11,23 +11,18 @@ def main():
         <title>Junac - POST/GET</title>
     </head>
     <body><h3>"""
-    if os.environ["QUERY_STRING"] == "":
-        #i = 0
-        """for line in sys.stdin.read():
-            if i == 0:
-                print "POST de POST/GET "
-            print line
-            i += 1
-        if i != 0:
-            print "Pas de chaine ni dans le GET ni dans le POST" """
+    try:
+        if os.environ["QUERY_STRING"] != "":
+            print "GET de POST/GET " + os.environ["QUERY_STRING"]
+        else:
+            raise KeyError
+    except KeyError:
         body = sys.stdin.read()
         if body != "":
             print "POST de POST/GET " + body
         else:
             print "Pas de chaine ni dans le GET ni dans le POST"
 
-    else:
-        print "GET de POST/GET " + os.environ["QUERY_STRING"]
     print """</h3></body>
     </html>"""
 
